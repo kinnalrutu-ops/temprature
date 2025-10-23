dfea43ff7a1479fdc1048ffe390032ea0d61779f
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-23 14:01:07 +0530

    save

--- temp_alert.py
+++ temp_alert.py
@@ -1,12 +1,18 @@
 
-temp = float(input("Enter the temperature in °C: "))
 
-if temp < 20:
+temp_c = float(input("Enter the temperature in °C: "))
+
+if temp_c < 20:
     status = "Cold"
-elif 20 <= temp <= 30:
+elif 20 <= temp_c <= 30:
     status = "Normal"
 else:
     status = "Hot"
 
-print("Temperature:", temp, "°C")
+
+temp_f = (temp_c * 9/5) + 32
+
+print("Temperature:", temp_c, "°C")
 print("Status:", status)
+print("Temperature in Fahrenheit:", round(temp_f, 2), "°F")
+
